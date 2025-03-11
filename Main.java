e8ea0348e8836bb1d5b502a85cc2c0b112e86382
VitorRosa11 <vitorrb1103@gmail.com>
2025-03-11 15:03:52 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,15 +1,27 @@
 
-package exercicios.pessoa;
+
+package exercicios.main;
 
 
 public class Main {
-    
- public static void main(String[] args) {
-        
-        Aluno aluno = new Aluno("Vitor Batista", 16, "A12345");
 
+    public static void main(String[] args) {
         
-        aluno.exibirInformacoes();
+package exerciciosobrecarga;
+
+public class Main {
+    public static void main(String[] args) {
+       
+        Calculadora calc = new Calculadora();
+
+       
+        int resultado1 = calc.multiplicar(5, 3);
+        int resultado2 = calc.multiplicar(2, 4, 6);
+        double resultado3 = calc.multiplicar(2.5, 3.2);
+
+       
+        System.out.println("Multiplicação de dois inteiros: " + resultado1);
+        System.out.println("Multiplicação de três inteiros: " + resultado2);
+        System.out.println("Multiplicação de dois doubles: " + resultado3);
     }
 }
-
